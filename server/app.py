e14bbbc44761bc1f0d60607c94e44bc9ba75ad14
Katya Weicht <katya.weicht@gmail.com>
2025-07-28 17:23:41 -0600

    Create signup endpoint

--- server/app.py
+++ server/app.py
@@ -14,8 +14,18 @@ class ClearSession(Resource):
         session['user_id'] = None
 
         return {}, 204
+    
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User(username=data['username'])
+        user.password_hash = data['password']
+        db.session.add(user)
+        db.session.commit()
+        return UserSchema().dump(user), 201
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
+api.add_resource(Signup, '/signup', endpoint='signup')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
